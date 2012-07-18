26e3b47cf3c97238d5bdb5ddd0b292653548e67f
pgottinger <peter@pgottinger.de>
2012-07-18 22:37:08 +0200

    Adapted LoginTests to new functionality.

--- test/controllers/LoginTest.java
+++ test/controllers/LoginTest.java
@@ -16,15 +16,13 @@ import static play.test.Helpers.session;
 import java.util.HashMap;
 import java.util.Map;
 
+import models.Configuration;
 import models.User;
 
-import org.fluentlenium.core.filter.matcher.ContainsMatcher;
-import org.junit.Ignore;
 import org.junit.Test;
 
-import play.mvc.Http.Context;
+import play.api.test.Helpers;
 import play.mvc.Result;
-import play.test.Helpers;
 import controllers.Login.LoginForm;
 
 public class LoginTest {
@@ -116,10 +114,18 @@ public class LoginTest {
 	}
 
 	@Test
-	public void testLogin() {
-		Result result = callAction(controllers.routes.ref.Login.login());
+	public void testLoginIfSystemIsInstalled() {
+		running(fakeApplication(inMemoryDatabase()), new Runnable() {
+			@Override
+			public void run() {
+				Configuration configuration = new Configuration(
+						Configuration.KEY_INSTALLED, "true");
+				configuration.save();
+				Result result = callAction(controllers.routes.ref.Login.login());
 
-		assertTrue(contentAsString(result).contains("<h1>Login</h1>"));
+				assertTrue(contentAsString(result).contains("<h1>Login</h1>"));
+			}
+		});
 	}
 
 	@Test
